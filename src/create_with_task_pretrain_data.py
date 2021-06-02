a0a0c1e780097254b202838a9fb42d55045e2b05
osuossu8 <osuosuossu18@gmail.com>
2021-06-02 10:05:16 +0000

    update

---
+++ src/create_with_task_pretrain_data.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append("/root/workspace/CommonLitReadabilityPrize")
+
+import numpy as np
+import pandas as pd
+
+from sklearn import datasets
+from sklearn import model_selection
+
+
+import pandas as pd
+import numpy as np
+
+train = pd.read_csv("inputs/train.csv")
+test = pd.read_csv("inputs/test.csv")
+
+mlm_data = train[['excerpt']]
+mlm_data = mlm_data.rename(columns={'excerpt':'text'})
+mlm_data.to_csv('inputs/mlm_data.csv', index=False)
+
+mlm_data_val = test[['excerpt']]
+mlm_data_val = mlm_data_val.rename(columns={'excerpt':'text'})
+mlm_data_val.to_csv('inputs/mlm_data_val.csv', index=False)
+
