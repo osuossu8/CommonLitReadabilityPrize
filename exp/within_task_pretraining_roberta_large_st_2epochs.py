7810ee5eec40820da5c6f95140854d8916283a9a
osuossu8 <osuosuossu18@gmail.com>
2021-06-11 00:48:33 +0000

    within_task_pretraining_roberta_large_st_2epochs

--- exp/within_task_pretraining_roberta_large_st_2epochs.py
+++ exp/within_task_pretraining_roberta_large_st_2epochs.py
@@ -54,7 +54,7 @@ class TrainConfig:
     gradient_accumulation_steps= 1
     lr_scheduler_type= 'linear' # 'constant_with_warmup'
     num_warmup_steps= 0
-    output_dir= 'itpt/roberta_large_squad_2'
+    output_dir= 'itpt/roberta_large_st'
     seed= 2021
     model_type= 'roberta'
     max_seq_length= None
