d9dc89c079901f96d2b6d00c11d80ef6b8b65c77
osuossu8 <osuosuossu18@gmail.com>
2021-06-14 02:39:25 +0000

    src/create_with_task_pretrain_data_4.py

---
+++ src/create_with_task_pretrain_data_4.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append("/root/workspace/CommonLitReadabilityPrize")
+
+import numpy as np
+import pandas as pd
+
+from sklearn import datasets
+from sklearn import model_selection
+
+
+import pandas as pd
+import numpy as np
+
+train = pd.read_csv("inputs/train.csv")
+test = pd.read_csv("inputs/test.csv")
+
+mlm_data = train[['excerpt']]
+mlm_data = mlm_data.rename(columns={'excerpt':'text'})
+mlm_data.to_csv('inputs/mlm_data.csv', index=False)
+
+mlm_data_val = test[['excerpt']]
+mlm_data_val = mlm_data_val.rename(columns={'excerpt':'text'})
+mlm_data_val.to_csv('inputs/mlm_data_val.csv', index=False)
+
+aug = pd.read_csv('inputs/children_books.csv')
+aug = aug[['Desc']]
+aug = aug.rename(columns={'Desc':'text'})
+
+mlm_data_aug = aug[['text']]
+
+new_mlm_data = pd.concat([mlm_data, mlm_data_aug], 0).reset_index(drop=True)
+new_mlm_data.to_csv('inputs/mlm_data_aug_children_data.csv', index=False)
