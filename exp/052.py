ad793fc0e882d5f5e1a6fe058a29e8c432d8541f
osuossu8 <osuosuossu18@gmail.com>
2021-06-14 15:48:04 +0000

    052

--- exp/052.py
+++ exp/052.py
@@ -41,7 +41,7 @@ class CFG:
     max_len = 256
     train_bs = 8 * 2
     valid_bs = 16 * 2
-    log_interval = 15 # 10
+    log_interval = 10
     model_name = 'roberta-large'
     itpt_path = 'itpt/roberta_large_2/'
 
