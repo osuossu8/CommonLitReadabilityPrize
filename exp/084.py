455b2fadb6562b747f92a6a013b99d76f881e119
osuossu8 <osuosuossu18@gmail.com>
2021-06-27 04:02:23 +0000

    084

--- exp/084.py
+++ exp/084.py
@@ -42,7 +42,7 @@ class CFG:
     train_bs = 8 * 2
     valid_bs = 16 * 2
     log_interval = 10
-    model_name = 'xlm-roberta-large' # 'roberta-large'
+    model_name = 'deepset/roberta-large-squad2' # 'roberta-large'
     itpt_path = None # 'itpt/roberta_large_2/' 
     numerical_cols = [
        'excerpt_num_chars', 'excerpt_num_capitals', 'excerpt_caps_vs_length',
