f0c4767e2c2a5560916d9930ff0707a6a9202f00
osuossu8 <osuosuossu18@gmail.com>
2021-06-28 15:44:40 +0000

    https://huggingface.co/siebert/sentiment-roberta-large-english

--- exp/087.py
+++ exp/087.py
@@ -42,7 +42,7 @@ class CFG:
     train_bs = 8 * 2
     valid_bs = 16 * 2
     log_interval = 10
-    model_name = 'howey/roberta-large-squad2' # 'navteca/roberta-large-squad2' # 'howey/roberta-large-mrpc' # 'roberta-large'
+    model_name = 'siebert/sentiment-roberta-large-english' # 'howey/roberta-large-squad2' # 'navteca/roberta-large-squad2' # 'howey/roberta-large-mrpc' # 'roberta-large'
     itpt_path = None # 'itpt/roberta_large_2/' 
     numerical_cols = [
        'excerpt_num_chars', 'excerpt_num_capitals', 'excerpt_caps_vs_length',
