54617ffeb04bbd81c4eaddf1ab260af5076bb21a
osuossu8 <osuosuossu18@gmail.com>
2021-07-01 15:01:30 +0000

    096

--- exp/096.py
+++ exp/096.py
@@ -42,7 +42,7 @@ class CFG:
     train_bs = 8 * 2
     valid_bs = 16 * 2
     log_interval = 10
-    model_name = 'klue/roberta-large'
+    model_name = 'ynie/roberta-large-snli_mnli_fever_anli_R1_R2_R3-nli'
     itpt_path = None # 'itpt/roberta_large_2/' 
     numerical_cols = [
        'excerpt_num_chars', 'excerpt_num_capitals', 'excerpt_caps_vs_length',
