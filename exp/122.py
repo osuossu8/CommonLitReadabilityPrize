93375975eab71198338952998f1c8a2d5cdfe1f9
osuossu8 <osuosuossu18@gmail.com>
2021-07-14 05:15:50 +0000

    122

--- exp/122.py
+++ exp/122.py
@@ -254,7 +254,7 @@ def aux_loss_fn(logits, targets):
 
 
 # https://github.com/dropreg/R-Drop
-def compute_kl_loss(p, q pad_mask=None):
+def compute_kl_loss(p, q, pad_mask=None):
     
     p_loss = F.kl_div(F.log_softmax(p, dim=-1), F.softmax(q, dim=-1), reduction='none')
     q_loss = F.kl_div(F.log_softmax(q, dim=-1), F.softmax(p, dim=-1), reduction='none')
