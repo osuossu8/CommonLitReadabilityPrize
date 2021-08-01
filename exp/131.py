cb3efbf0c7d59e1914552c253dd500290658e826
osuossu8 <osuosuossu18@gmail.com>
2021-08-01 14:21:45 +0000

    131

--- exp/131.py
+++ exp/131.py
@@ -42,7 +42,7 @@ class CFG:
     train_bs = 8 * 2
     valid_bs = 16 * 2
     log_interval = 10
-    model_name = 'distilbert-base-uncased-distilled-squad'
+    model_name = 'deepset/roberta-base-squad2'
     itpt_path = None # 'itpt/roberta_large_2/' 
     numerical_cols = [
        'excerpt_num_chars', 'excerpt_num_capitals', 'excerpt_caps_vs_length',
